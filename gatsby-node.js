d818c7e1e2b421e29666857c3159b48342c61a15
Ben Robertson <hi@benrobertson.io>
2022-07-31 14:13:34 -0400

    Redirect blog posts.

--- gatsby-node.js
+++ gatsby-node.js
@@ -2,38 +2,39 @@ const path = require('path');
 require("dotenv").config({
   path: `.env.${process.env.NODE_ENV}`,
 })
-const { copyLibFiles } = require('@builder.io/partytown/utils');
 
-exports.onPreBuild = async () => {
-  await copyLibFiles(path.join(__dirname, 'static', '~partytown'));
-};
 
 
 exports.createPages = async ({ actions, graphql, reporter }) => {
-  const { createPage, createRedirect } = actions;
+  const { createRedirect } = actions;
 
 
-  createRedirect({
-    fromPath: `/*`,
-    toPath: `https://ben.robertson.is/*`,
-    // isPermanent: true
-  })
+  const result = await graphql(`
+  query {
+    allMdx(filter: {frontmatter: {path: {ne: "null"}}}) {
+      edges {
+        node {
+          id
+          frontmatter {
+            path
+          }
+        }
+      }
+    }
+  }
+`);
+  if (result.errors) {
+    reporter.panicOnBuild('🚨  ERROR: Loading "createPages" query');
+  }
+  // Create blog post pages.
+  const posts = result.data.allMdx.edges;
+  // you'll call `createPage` for each result
+  posts.forEach(({ node }) => {
+    if (!node.frontmatter.path) return;
 
-  createRedirect({
-    fromPath: `/`,
-    toPath: `https://ben.robertson.is/`,
-    // isPermanent: true
-  })
-
-  // Reverse proxy for google tag manager & partytown.
-  createRedirect({
-    fromPath: `/gtm/*`,
-    toPath: `https://www.googletagmanager.com/*`,
-    statusCode: 200,
-  })
-  createRedirect({
-    fromPath: `/googleanalytics/*`,
-    toPath: `https://www.google-analytics.com/*`,
-    statusCode: 200,
-  })
+    createRedirect({
+      fromPath: `https://benrobertson.io${node.frontmatter.path}/`,
+      toPath: `https://ben.robertson.is${node.frontmatter.path}`
+    })
+  });
 };
