7f31fc5e62f73e7d88390e45e180a8555d1e9fba
Ben Robertson <hi@benrobertson.io>
2022-11-16 19:19:11 -0500

    Remove google analytics

--- gatsby-node.js
+++ gatsby-node.js
@@ -2,12 +2,6 @@ const path = require('path');
 require("dotenv").config({
   path: `.env.${process.env.NODE_ENV}`,
 })
-const { copyLibFiles } = require('@builder.io/partytown/utils');
-
-exports.onPreBuild = async () => {
-  await copyLibFiles(path.join(__dirname, 'static', '~partytown'));
-};
-
 
 exports.createPages = async ({ actions, graphql, reporter }) => {
   const { createPage, createRedirect } = actions;
@@ -51,20 +45,6 @@ exports.createPages = async ({ actions, graphql, reporter }) => {
 
   });
 
-
-
-
-  // Reverse proxy for google tag manager & partytown.
-  createRedirect({
-    fromPath: `/gtm/*`,
-    toPath: `https://www.googletagmanager.com/*`,
-    statusCode: 200,
-  })
-  createRedirect({
-    fromPath: `/googleanalytics/*`,
-    toPath: `https://www.google-analytics.com/*`,
-    statusCode: 200,
-  })
 
   // Reverse proxy for google tag manager & partytown.
   createRedirect({
